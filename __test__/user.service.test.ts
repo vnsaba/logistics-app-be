a0c7bc18a258268a4bbf8ae37b902f839864b028
sbsmrth <andresf.mrh@gmail.com>
2025-04-23 16:50:16 -0500

    feat: basic tests example

---
+++ __test__/user.service.test.ts
@@ -0,0 +1,51 @@
+import { UserService } from '../src/auth-service/application/user.service';
+import { IUserRepository } from '../src/auth-service/domain/interfaces/user.interface';
+import { User } from '../src/auth-service/domain/user';
+
+class MockUserRepository implements IUserRepository {
+  private users: User[] = [];
+
+  async createUser(user: User): Promise<User> {
+    this.users.push(user);
+    return user;
+  }
+}
+
+describe('UserService', () => {
+  let userService: UserService;
+
+  beforeEach(() => {
+    const mockUserRepository = new MockUserRepository();
+    userService = new UserService(mockUserRepository);
+  });
+
+  test('Add the user to the mock db', async () => {
+    const userData = {
+      fullname: 'John Doe',
+      email: 'john@gmail.com',
+      current_password: 'password123',
+      status: 'ACTIVE',
+      roleId: '6808560b7c58de59c8a68615',
+    };
+    const user: User = new User(
+      userData.fullname,
+      userData.email,
+      userData.current_password,
+      userData.status,
+      userData.roleId
+    );
+
+    const addedUser = await userService.createUser(
+      userData.fullname,
+      userData.email,
+      userData.current_password,
+      userData.status,
+      userData.roleId
+    );
+
+    expect(addedUser.email).toBe(user.email);
+    expect(addedUser.fullname).toBe(user.fullname);
+
+    // usar jest mock para testear infraestructura, acá solo logica de negocio
+  });
+});
